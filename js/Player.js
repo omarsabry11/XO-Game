d2380a05a24f9dd7110a095a96ecbc10a89d7ab5
Omar <omarsabry425@gmail.com>
2025-06-08 00:59:35 +0300

    Improving grid 3*3 type

--- js/Player.js
+++ js/Player.js
@@ -1,10 +1,6 @@
-export class Player
-{
-    constructor(name,sign,score)
-    {
-        this.name = name;
-        this.sign = sign;
-        this.score = score;
-    }
-
+export class Player {
+  score;
+  constructor() {
+    this.score = 0;
+  }
 }
--- js/game3.js
+++ js/game3.js
@@ -1,353 +1,286 @@
-"use strict"
+"use strict";
 
+import { Player } from "./Player.js";
+let boxes = document.querySelectorAll(".box3");
 
-import { Player } from './Player.js'
-let boxes = document.querySelectorAll('.box3');
-
-let containerBox = document.querySelector('.container-box');
-let lightBox = document.getElementById('light-box');
-
-
+let containerBox = document.querySelector(".container-box");
+let lightBox = document.getElementById("light-box");
 let x = `<h2 class="x neon-text m-0 p-0 position-absolute top-50 start-50 translate-middle">X</h2>`;
 let o = `<h2 class="o neon-text m-0 p-0 position-absolute top-50 start-50 translate-middle">O</h2>`;
 let xTurn = true;
-let container = [];
 let count = 0;
-container.length = 9;
-
-let score1 = document.getElementById('score1');
-let score2 = document.getElementById('score2');
-
+let score1 = document.getElementById("score1");
+let score2 = document.getElementById("score2");
 
 let player1 = new Player();
 let player2 = new Player();
 
-player1.score = 0;
-player2.score = 0;
+const grid = [
+  [false, false, false],
+  [false, false, false],
+  [false, false, false],
+];
 
-
-for (let i = 0; i < container.length; i++) {
-    container[i] = false;
-}
-
-let flag = false;
 let char;
 let position;
 let index;
-let xNewTurn = true;
-
 
 
 $("#container-box").hide();
 
+const resetGrid = () => {
+  for (let i = 0; i < 3; i++) {
+    for (let j = 0; j < 3; j++) {
+      grid[i][j] = false;
+    }
+  }
+};
 
-$('#play-again .yes').on('click', function () {
-    document.getElementById('play-again').classList.replace('d-flex', 'd-none');
-    document.getElementById('same-players').classList.replace('d-none', 'd-flex');
-    reset();
+const setDrawBox = () => {
+  hideLightBox();
+  document.querySelector("#light-box h2.sign").innerHTML = "";
+  document.querySelector("#light-box h2.status").innerHTML = "Draw";
+  $("#container-box")
+    .delay(300)
+    .show(0, function () {
+      $("#light-box").animate({ width: "22rem", height: "15rem" }, function () {
+        $("#light-box .content").animate({ scale: 1 }, 300);
+      });
+    });
+};
 
-})
+const handleWinning = (winner) => {
+  setWinLines();
+  winner == "player1" ? player1.score++ : player2.score++;
+  winner == "player1"
+    ? (score1.textContent = player1.score)
+    : (score2.textContent = player2.score);
+  hideLightBox();
+  document.querySelector("#light-box h2.sign").innerHTML = `${
+    winner == "player1" ? "X" : "O"
+  }`;
+  document.querySelector("#light-box h2.status").innerHTML = "Winner";
+  document.querySelector("#light-box h2.sign").style.color = `${
+    winner == "player1" ? "#08aaaa" : "#e36464"
+  }`;
 
-$('#same-players .yes').on('click', function () {
+  $("#container-box")
+    .delay(550)
+    .show(0, function () {
+      $("#light-box").animate({ width: "22rem", height: "18rem" }, function () {
+        $("#light-box .content").animate({ scale: 1 }, 300);
+      });
+    });
 
-    document.getElementById('play-again').classList.replace('d-none', 'd-flex');
-    document.getElementById('same-players').classList.replace('d-flex', 'd-none');
-    $("#container-box").hide();
-})
-$('#same-players .no').on('click', function () {
+  for (let i = 0; i < 3; i++) {
+    for (let j = 0; j < 3; j++) {
+      grid[i][j] = true;
+    }
+  }
+};
 
-    document.getElementById('play-again').classList.replace('d-none', 'd-flex');
-    document.getElementById('same-players').classList.replace('d-flex', 'd-none');
-    player1.score = 0;
-    player2.score = 0;
-    score1.textContent = 0;
-    score2.textContent = 0;
-    $("#container-box").hide();
-})
+$("#play-again .yes").on("click", function () {
+  document.getElementById("play-again").classList.replace("d-flex", "d-none");
+  document.getElementById("same-players").classList.replace("d-none", "d-flex");
+  reset();
+});
 
-document.querySelector('.result span').innerHTML = `X`;
+$("#same-players .yes").on("click", function () {
+  document.getElementById("play-again").classList.replace("d-none", "d-flex");
+  document.getElementById("same-players").classList.replace("d-flex", "d-none");
+  $("#container-box").hide();
+});
+$("#same-players .no").on("click", function () {
+  document.getElementById("play-again").classList.replace("d-none", "d-flex");
+  document.getElementById("same-players").classList.replace("d-flex", "d-none");
+  player1.score = 0;
+  player2.score = 0;
+  score1.textContent = 0;
+  score2.textContent = 0;
+  $("#container-box").hide();
+});
 
+document.querySelector(".result span").innerHTML = `X`;
 
 for (let i = 0; i < boxes.length; i++) {
-    boxes[i].addEventListener('click', function () {
+  boxes[i].addEventListener("click", function () {
+    const row = Math.floor(i / 3);
+    const col = i % 3;
 
+    if (!grid[row][col]) {
+      if (xTurn) {
+        grid[row][col] = "X";
+        boxes[i].innerHTML = x;
+        count++;
+        xTurn = !xTurn;
+        document.querySelector(".result span").innerHTML = `${
+          xTurn ? "X" : "O"
+        }`;
 
-        if (xTurn) {
-
-            if (container[i] == false) {
-                boxes[i].innerHTML = x;
-                container[i] = 'X';
-                count++;
-                xTurn = !xTurn;
-                document.querySelector('.result span').innerHTML = `${xTurn ? 'X' : 'O'}`;
-            }
-
-            if (isWin() == true) {
-
-
-                setWinLines();
-                player1.score++;
-                score1.textContent = player1.score;
-                setLightBox();
-                document.querySelector('#light-box h2.sign').innerHTML = "X";
-                document.querySelector('#light-box h2.status').innerHTML = "Winner";
-                document.querySelector('#light-box h2.sign').style.color = '#08aaaa'
-
-                $("#container-box").delay(550).show(0, function () {
-
-                    $('#light-box').animate({ width: '22rem', height: '18rem' }, function () {
-                        $('#light-box .content').animate({ scale: 1 }, 300);
-                    });
-
-                });
-
-                for (let i = 0; i < container.length; i++) {
-                    container[i] = true;
-                }
-
-
-            }
-            else if (count == 9 && !isWin()) {
-
-                setLightBox();
-                document.querySelector('#light-box h2.sign').innerHTML = "";
-                document.querySelector('#light-box h2.status').innerHTML = "Draw";
-                $("#container-box").delay(300).show(0, function () {
-
-                    $('#light-box').animate({ width: '22rem', height: '15rem' }, function () {
-                        $('#light-box .content').animate({ scale: 1 }, 300);
-                    });
-
-                });
-
-            }
-
-
+        if (isWin()) {
+          handleWinning("player1");
+        } else if (count == 9 && !isWin()) {
+          setDrawBox();
         }
-
-        else {
-
-
-            if (container[i] == false) {
-                boxes[i].innerHTML = o;
-                container[i] = 'O';
-                count++;
-                xTurn = !xTurn;
-                document.querySelector('.result span').innerHTML = `${xTurn ? 'X' : 'O'}`;
-
-            }
-            if (isWin() == true) {
-
-               setWinLines();
-                player2.score++;
-                score2.textContent = `${player2.score}`;
-                setLightBox();
-                document.querySelector('#light-box h2.sign').innerHTML = "O";
-                document.querySelector('#light-box h2.status').innerHTML = "Winner";
-                document.querySelector('#light-box h2.sign').style.color = '#e36464'
-                $("#container-box").delay(550).show(0, function () {
-
-                    $('#light-box').animate({ width: '22rem', height: '18rem' }, function () {
-                        $('#light-box .content').animate({ scale: 1 }, 300);
-                    });
-
-                });
-
-                for (let i = 0; i < container.length; i++) {
-                    container[i] = true;
-                }
-
-            }
-            else if (count == 9 && !isWin()) {
-                setLightBox();
-
-
-                document.querySelector('#light-box h2.sign').innerHTML = "";
-                document.querySelector('#light-box h2.status').innerHTML = "Draw";
-                $("#container-box").delay(300).show(0, function () {
-
-                    $('#light-box').animate({ width: '22rem', height: '15rem' }, function () {
-                        $('#light-box .content').animate({ scale: 1 }, 300);
-                    });
-
-                });
-
-
-
-
-            }
-
+      } else {
+        grid[row][col] = "O";
+        boxes[i].innerHTML = o;
+        count++;
+        xTurn = !xTurn;
+        document.querySelector(".result span").innerHTML = `${
+          xTurn ? "X" : "O"
+        }`;
+        if (isWin()) {
+          handleWinning("player2");
+        } else if (count == 9 && !isWin()) {
+          setDrawBox();
         }
-
-    })
+      }
+    }
+  });
 }
-
-
-
-
-
-
 
 function isWin() {
-    let i;
-    for (i = 0; i <= 6; i += 3) {
-        if (container[i] == container[i + 1] &&
-            container[i] == container[i + 2] &&
-            (container[i] == 'X' || container[i] == 'O')) {
-            char = container[i];
-            index = i;
-            position = 'h'
-            return true;
-        }
-    }
-    for (i = 0; i <= 2; i++) {
-        if (container[i] == container[i + 3] &&
-            container[i] == container[i + 6] &&
-            (container[i] == 'X' || container[i] == 'O')) {
-            char = container[i];
-            index = i;
-            position = 'v'
-            return true;
-        }
-    }
-    i = 0;
-    if (container[i] == container[i + 4] &&
-        container[i] == container[i + 8] &&
-        (container[i] == 'X' || container[i] == 'O')) {
-        char = container[i];
-        index = i;
-        position = 'mD'
-        return true;
-    }
-    i = 2
-    if (container[i] == container[i + 2] &&
-        container[i] == container[i + 4] &&
-        (container[i] == 'X' || container[i] == 'O')) {
-        char = container[i];
-        index = i;
-        position = 'sD'
-        return true;
+  //Horizontal Lines
+  if (
+    grid[0][0] == grid[0][1] &&
+    grid[0][0] == grid[0][2] &&
+    (grid[0][0] == "X" || grid[0][0] == "O")
+  ) {
+    index = 0;
+    char = grid[0][0];
+    position = "h";
+    return true;
+  }
+  if (
+    grid[1][0] == grid[1][1] &&
+    grid[1][0] == grid[1][2] &&
+    (grid[1][0] == "X" || grid[1][0] == "O")
+  ) {
+    index = 3;
+    char = grid[1][0];
+    position = "h";
+    return true;
+  }
+  if (
+    grid[2][0] == grid[2][1] &&
+    grid[2][0] == grid[2][2] &&
+    (grid[2][0] == "X" || grid[2][0] == "O")
+  ) {
+    index = 6;
+    char = grid[2][0];
+    position = "h";
+    return true;
+  }
 
-    }
+  //Vertical Lines
+  if (
+    grid[0][0] == grid[1][0] &&
+    grid[0][0] == grid[2][0] &&
+    (grid[0][0] == "X" || grid[0][0] == "O")
+  ) {
+    index = 0;
+    char = grid[0][0];
+    position = "v";
+    return true;
+  }
+  if (
+    grid[0][1] == grid[1][1] &&
+    grid[0][1] == grid[2][1] &&
+    (grid[0][1] == "X" || grid[0][1] == "O")
+  ) {
+    index = 1;
+    char = grid[0][1];
+    position = "v";
+    return true;
+  }
+  if (
+    grid[0][2] == grid[1][2] &&
+    grid[0][2] == grid[2][2] &&
+    (grid[0][2] == "X" || grid[0][2] == "O")
+  ) {
+    index = 2;
+    char = grid[0][1];
+    position = "v";
+    return true;
+  }
 
-    return false;
+  //Main Dialog Lines
+  if (
+    grid[0][0] == grid[1][1] &&
+    grid[0][0] == grid[2][2] &&
+    (grid[0][0] == "X" || grid[0][0] == "O")
+  ) {
+    char = grid[0][1];
+    position = "md";
+    return true;
+  }
+  //Second Dialog Lines
+  if (
+    grid[0][2] == grid[1][1] &&
+    grid[0][2] == grid[2][0] &&
+    (grid[0][2] == "X" || grid[0][2] == "O")
+  ) {
+    char = grid[0][1];
+    position = "sd";
+    return true;
+  }
+
+  return false;
 }
-
-
 
 function reset() {
-    for (let i = 0; i < container.length; i++) {
-        container[i] = false;
-    }
-    for (let i = 0; i < boxes.length; i++) {
-        boxes[i].innerHTML = '';
-    }
+  for (let i = 0; i < boxes.length; i++) {
+    boxes[i].innerHTML = "";
+  }
+  resetGrid();
 
-    count = 0;
-    xNewTurn = !xNewTurn;
-    xTurn = xNewTurn;
+  count = 0;
+  document.querySelector(".wH").style.width = "0%";
+  document.querySelector(".wV").style.height = "0%";
 
-    document.querySelector('.wH').style.width = '0%';
-    document.querySelector('.wV').style.height = '0%';
-   
-
-
-
-    let st = document.getElementById('game3');
-    st.style.setProperty('--beforeBack', '0%');
-
-    let st2 = document.getElementById('game3');
-    st.style.setProperty('--afterBack', '0%');
-
+  let st = document.getElementById("game3");
+  st.style.setProperty("--beforeBack", "0%");
+  st.style.setProperty("--afterBack", "0%");
 }
 
-function setLightBox() {
-    $('#container-box').hide();
-    lightBox.style.width = 0;
-    lightBox.style.height = 0;
-    document.querySelector('#light-box .content').style.scale = 0;
-
+function hideLightBox() {
+  $("#container-box").hide();
+  lightBox.style.width = 0;
+  lightBox.style.height = 0;
+  document.querySelector("#light-box .content").style.scale = 0;
 }
-
-
-
 
 function setWinLines() {
-    if (position == 'v') {
-
-        if (index == 0) {
-
-
-            document.querySelector('.wV').style.left = '16.667%';
-            document.querySelector('.wV').style.height = '100%';
-
-
-        }
-        if (index == 1) {
-
-            document.querySelector('.wV').style.left = '50%';
-            document.querySelector('.wV').style.height = '100%';
-
-
-
-        }
-        if (index == 2) {
-
-            document.querySelector('.wV').style.left = '83.333%';
-            document.querySelector('.wV').style.height = '100%';
-
-
-
-        }
-
-
-
-
+  if (position == "h") {
+    if (index == 0) {
+      document.querySelector(".wH").style.top = "16.667%";
+      document.querySelector(".wH").style.width = "100%";
+    } else if (index == 3) {
+      document.querySelector(".wH").style.top = "50%";
+      document.querySelector(".wH").style.width = "100%";
+    } else if (index == 6) {
+      document.querySelector(".wH").style.top = "83.33%";
+      document.querySelector(".wH").style.width = "100%";
     }
-
-
-    if (position == 'h') {
-
-        if (index == 0) {
-
-
-            document.querySelector('.wH').style.top = '16.667%';
-            document.querySelector('.wH').style.width = '100%';
-
-
-
-
-        }
-        else if (index == 3) {
-
-            document.querySelector('.wH').style.top = '50%';
-            document.querySelector('.wH').style.width = '100%';
-
-
-
-        }
-        else if (index == 6) {
-
-            document.querySelector('.wH').style.top = '83.33%';
-            document.querySelector('.wH').style.width = '100%';
-
-
-
-        }
-
-
-
+  } else if (position == "v") {
+    if (index == 0) {
+      document.querySelector(".wV").style.left = "16.667%";
+      document.querySelector(".wV").style.height = "100%";
     }
-
-    if (position == 'mD') {
-        let st = document.getElementById('game3');
-        st.style.setProperty('--beforeBack', '141.42%');
-
+    if (index == 1) {
+      document.querySelector(".wV").style.left = "50%";
+      document.querySelector(".wV").style.height = "100%";
     }
-    if (position == 'sD') {
-
-        let st = document.getElementById('game3');
-        st.style.setProperty('--afterBack', '141.42%')
+    if (index == 2) {
+      document.querySelector(".wV").style.left = "83.333%";
+      document.querySelector(".wV").style.height = "100%";
     }
-
+  } else if (position == "md") {
+    let st = document.getElementById("game3");
+    st.style.setProperty("--beforeBack", "141.42%");
+  } else if (position == "sd") {
+    let st = document.getElementById("game3");
+    st.style.setProperty("--afterBack", "141.42%");
+  }
 }
